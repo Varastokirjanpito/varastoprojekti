e0e2c30561bca01246ed41b53d2a9ada4445d21e
valtov123 <60961213+valtov123@users.noreply.github.com>
2020-02-12 11:34:56 +0200

    Add files via upload

---
+++ testi666/src/testi666/testiii.java
@@ -0,0 +1,17 @@
+package testi666;
+
+public class testiii{  
+	
+	
+	
+	
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+     
+     for (int i = 0; i<10; i++) {
+    	 System.out.println("HELLO");
+     }
+     
+     
+    }  
+}  
